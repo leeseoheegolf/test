f0bb079fad9992d4639b12ceaa2c71de4abcb5bd
LeeSeohee2 <golfchild8069@gmail.com>
2024-11-07 16:30:31 +0900

    .

--- Lostay/backend/src/test/java/com/lostay/backend/BackendApplicationTests.java
+++ Lostay/backend/src/test/java/com/lostay/backend/BackendApplicationTests.java
@@ -2,7 +2,7 @@ package com.lostay.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Disabled
 @SpringBootTest
 class BackendApplicationTests {
 
